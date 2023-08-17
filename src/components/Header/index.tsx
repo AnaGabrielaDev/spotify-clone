50505f2af7282ef7853e5946e3602f621adbb07f
Ana Gabriela <anagabrieladev@gmil.com>
2023-08-17 19:29:46 -0300

    feat: add font roboto by default

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 export function Header() {
     return (
-        <header className="bg-zinc-900 text-white p-4">
-            <div className="w-10/12 mx-auto flex justify-between">
+        <header className="bg-zinc-900 text-white h-20 flex">
+            <div className="w-10/12 mx-auto flex justify-between items-center">
                 <img src="/spotify.svg" alt="logo" className="invert h-10"/>
-                <nav className="flex items-center">
-                    <ul className="flex gap-4 font-bold">
-                        <li className="hover:text-green-500">Premium</li>
-                        <li className="hover:text-green-500">Suporte</li>
-                        <li className="hover:text-green-500">Baixar</li>
+                <nav className="flex items-center mr-8 text-base">
+                    <ul className="flex gap-9">
+                        <li className="font-bold hover:text-green-500">Premium</li>
+                        <li className="font-bold hover:text-green-500">Suporte</li>
+                        <li className="font-bold hover:text-green-500">Baixar</li>
                         <li>|</li>
                         <li className="hover:text-green-500">Inscrever-se</li>
                         <li className="hover:text-green-500">Entrar</li>
