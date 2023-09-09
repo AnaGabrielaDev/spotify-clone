4547a2aa2f2aef56870eb0357714a4800935f2c6
felipe <felipecassianobarbosa@hotmail.com>
2023-09-09 15:28:43 -0300

    feat: add handleSubmit prop

--- src/components/Form/FormWrapper.tsx
+++ src/components/Form/FormWrapper.tsx
@@ -3,16 +3,17 @@ import { twMerge } from "tailwind-merge"
 
 interface FormWrapperInterface {
     classNameProps?: HTMLHeadingElement,
-    children: ReactNode
+    children: ReactNode,
+    handleSubmit: () => void
 }
 
-function FormWrapper({children, classNameProps}: FormWrapperInterface) {
+function FormWrapper({children, classNameProps, handleSubmit}: FormWrapperInterface) {
     return (
-        <header className={twMerge("flex")}>
+        <form className={twMerge("flex")} onSubmit={handleSubmit}>
             <div className="my-20 mx-auto justify-between items-center">
                 {children}
             </div>
-        </header>
+        </form>
     )
 }
 
