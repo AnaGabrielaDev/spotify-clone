cf86d4b659ddc0fccd092d3bc0105b01e6a2c6f1
felipe <felipecassianobarbosa@hotmail.com>
2023-09-09 15:28:25 -0300

    feat: add focus

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -1,7 +1,7 @@
 function Button({ text, width, handleOnClick }: { text: string, width: string, handleOnClick: () => void}) {
     return (
         <>
-            <button className={`mx-1 my-3 border-2 w-[${width}px] border-green-500 hover:bg-green-500 hover:text-white rounded-lg`} type="submit" onClick={handleOnClick}>
+            <button className={`mx-1 my-3 border-2 w-[${width}px] border-green-500 hover:bg-green-500 hover:text-white rounded-lg focus:bg-green-500`} type="submit" onClick={handleOnClick}>
                 <p className="mx-1">{text}</p>
             </button>
         </>
