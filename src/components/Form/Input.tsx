a3b81a0902b3236608367320909ec3e3b4235bd2
felipe <felipecassianobarbosa@hotmail.com>
2023-09-09 15:28:59 -0300

    refactor: change text color

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -2,7 +2,7 @@ function Input({label, type, name}: {label: string, type: string, name: string})
     return (
         <>
             <label className="block font-bold text-lg">{label}</label>
-            <input type={type} name={name} id={name} className="border-2 border-green-500 w-[400px] focus:border-white-100" placeholder="..."/>
+            <input type={type} name={name} id={name} className="border-2 border-green-500 text-black w-[400px] focus:border-white-100" placeholder="..."/>
         </>
     );
 }
