89d50518cf207c13e239c2f75373fec806c8eb29
AnaGabriela <anagabrieladev@gmail.com>
2023-10-10 10:36:39 -0300

    feat: link logo to home

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router-dom";
+
 export function Logo() {
     return (
-        <img src="/spotify.svg" alt="logo" className="invert h-10"/>
+        <Link to="/" replace={true}>
+            <img src="/spotify.svg" alt="logo" className="invert h-10"/>
+        </Link>
     )
 }
